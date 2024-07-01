c365d12423a2b0ebd301084218675893410d5e98
Tarun Bharti <tarun@backbase.com>
2024-07-01 11:39:50 +0200

    Fix test cases

--- src/test/java/org/custom/LogBeforeHandleExceptionCheckerTest.java
+++ src/test/java/org/custom/LogBeforeHandleExceptionCheckerTest.java
@@ -1,14 +1,16 @@
 package org.custom;
 
+import autovalue.shaded.com.google.common.base.Predicates;
 import com.google.errorprone.CompilationTestHelper;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 
 public class LogBeforeHandleExceptionCheckerTest {
 
     private CompilationTestHelper compilationTestHelper;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         compilationTestHelper = CompilationTestHelper.newInstance(LogBeforeHandleExceptionChecker.class, getClass());
     }
@@ -16,48 +18,49 @@ public class LogBeforeHandleExceptionCheckerTest {
     @Test
     public void testLogBeforeHandleException() {
         compilationTestHelper
-                .addSourceLines(
-                        "Test.java",
-                        "import java.io.IOException;",
-                        "import org.slf4j.Logger;",
-                        "import org.slf4j.LoggerFactory;",
-                        "import org.springframework.web.client.HttpClientErrorException;",
-                        "public class Test {",
-                        "    private static final Logger log = LoggerFactory.getLogger(Test.class);",
-                        "    public void testMethod() {",
-                        "        try {",
-                        "            // some code",
-                        "        } catch (HttpClientErrorException ex) {",
-                        "            handleException(ex);",  // Incorrect
-                        "            log.error(\"Error reading file\", ex);",
-                        "        }",
-                        "    }",
-                        "    private void handleException(HttpClientErrorException e) {}",
-                        "}")
-                .doTest();
+            .addSourceLines(
+                "Test.java",
+                "import java.io.IOException;",
+                "import org.slf4j.Logger;",
+                "import org.slf4j.LoggerFactory;",
+                "import java.lang.Exception;",
+                "public class Test {",
+                "    private static final Logger log = LoggerFactory.getLogger(Test.class);",
+                "    public void testMethod() {",
+                "        try {",
+                "            // some code",
+                "        } catch (Exception ex) {",
+                "            handleException(ex);",  // Incorrect
+                "            log.error(\"Error reading file\", ex);",
+                "        }",
+                "    }",
+                "    private void handleException(Exception e) {}",
+                "}")
+            .doTest();
+
     }
 
     @Test
     public void testCorrectLogBeforeHandleException() {
         compilationTestHelper
-                .addSourceLines(
-                        "Test.java",
-                        "import java.io.IOException;",
-                        "import org.slf4j.Logger;",
-                        "import org.slf4j.LoggerFactory;",
-                        "import org.springframework.web.client.HttpClientErrorException;",
-                        "public class Test {",
-                        "    private static final Logger log = LoggerFactory.getLogger(Test.class);",
-                        "    public void testMethod() {",
-                        "        try {",
-                        "            // some code",
-                        "        } catch (HttpClientErrorException ex) {",
-                        "            log.error(\"Error reading file\", ex);", // Correct
-                        "            handleException(ex);",
-                        "        }",
-                        "    }",
-                        "    private void handleException(HttpClientErrorException e) {}",
-                        "}")
-                .doTest();
+            .addSourceLines(
+                "Test.java",
+                "import java.io.IOException;",
+                "import org.slf4j.Logger;",
+                "import org.slf4j.LoggerFactory;",
+                "import java.lang.Exception;",
+                "public class Test {",
+                "    private static final Logger log = LoggerFactory.getLogger(Test.class);",
+                "    public void testMethod() {",
+                "        try {",
+                "            // some code",
+                "        } catch (Exception ex) {",
+                "            log.error(\"Error reading file\", ex);", // Correct
+                "            handleException(ex);",
+                "        }",
+                "    }",
+                "    private void handleException(Exception e) {}",
+                "}")
+            .doTest();
     }
 }
